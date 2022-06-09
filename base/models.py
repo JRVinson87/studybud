d093658cf3f7bcb55de86ddd165eef8caa54a728
Joshua Vinson <JRVinson87@gmail.com>
2022-06-08 19:38:08 -0600

    initialize migration sqlite and room model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    #host
+    #topic
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True) #null True: it is allowed to be blank
+    #participants = 
+    updated = models.DateTimeField(auto_now=True) #timestamp every time
+    created = models.DateTimeField(auto_now_add=True) #timestamp when created
+
+    def __str__(self):
+        return self.name
+
+    
