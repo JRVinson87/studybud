8d89b961b5213c239fc15824c3ceaccaff6f6cf0
Joshua Vinson <JRVinson87@gmail.com>
2022-06-09 18:19:13 -0600

    add delete room URL path

--- base/views.py
+++ base/views.py
@@ -49,6 +49,7 @@ def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
     if request.method == 'POST':
         room.delete()
+        return redirect('home')
 
     return render(request, 'base/delete.html', {'obj': room})
 
